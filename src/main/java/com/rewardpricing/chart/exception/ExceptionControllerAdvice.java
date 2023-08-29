f4b4a49116d68c306340012f774dd1b8d012cfc2
luishernan.espinosa <luis.espinosallanos>
2023-08-28 19:22:35 -0500

    changes

--- src/main/java/com/rewardpricing/chart/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/rewardpricing/chart/exception/ExceptionControllerAdvice.java
@@ -4,16 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import com.rewardpricing.chart.dto.ErrorMessageDto;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
-
+  
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public ResponseEntity handleMaxSizeException(MaxUploadSizeExceededException exc) {
-    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new ErrorMessageDto("File too large!"));
   }
   
